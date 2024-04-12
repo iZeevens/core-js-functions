8ba454b2c47885000e8fc26f3467b404149026fe
iZeevens <129598659+iZeevens@users.noreply.github.com>
2024-04-12 23:51:15 +0500

    feat: add getIdGeneratorFunction

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -241,8 +241,13 @@ function partialUsingArguments(fn, ...args1) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  const cache = {};
+  cache[startFrom] = startFrom - 1;
+  return function () {
+    cache[startFrom] += 1;
+    return cache[startFrom];
+  };
 }
 
 module.exports = {
